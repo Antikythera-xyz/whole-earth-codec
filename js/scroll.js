4849b500037046b90007c0774a38280b2e225a8a
Christina Lu <c.20@dartmouth.edu>
2023-03-10 04:12:18 -0800

    arrange case studies and add conclusion

--- js/scroll.js
+++ js/scroll.js
@@ -63,3 +63,48 @@ var onDataScroll = function() {
   dataDiagram.src = newImageUrl;
 };
 window.addEventListener("scroll", onDataScroll);
+
+// CODEC SCROLL
+codec = document.getElementById("codec");
+codecDisappear = document.getElementById("codecDisappear");
+var onCodecScroll = function() {
+  var scrollTop = document.body.scrollTop + 2*window.innerHeight/3;
+  if (scrollTop > codecDisappear.getBoundingClientRect().top) {
+     codec.className = "sticky hidden";
+  } else {
+     codec.className = "sticky visible";
+  }
+};
+window.addEventListener("scroll", onCodecScroll);
+
+// CASE STUDY SCROLL
+finetunedDiagram = document.getElementById("finetunedDiagram");
+finetuned1 = document.getElementById("finetuned1");
+finetuned2 = document.getElementById("finetuned2");
+finetuned3 = document.getElementById("finetuned3");
+finetuned4 = document.getElementById("finetuned4");
+caseDisappear = document.getElementById("caseDisappear");
+var onCaseScroll = function() {
+  var newImageUrl = finetuned.src;
+  var scrollTop = document.body.scrollTop + window.innerHeight/2;
+  if (scrollTop > finetuned1.getBoundingClientRect().top) {
+     newImageUrl = "img/Diagram6_v1.png";
+     finetunedDiagram.className = "visible";
+     caseDisappear.className = "hidden";
+  }
+  if (scrollTop > finetuned2.getBoundingClientRect().top) {
+     newImageUrl = "img/Diagram7_v1.png";
+  }
+  if (scrollTop > finetuned3.getBoundingClientRect().top) {
+     newImageUrl = "img/Diagram8_v1.png";
+  }
+  if (scrollTop < finetuned1.getBoundingClientRect().top) {
+     finetunedDiagram.className = "hidden";
+     caseDisappear.className = "visible";
+  }
+  if (scrollTop > finetuned4.getBoundingClientRect().top) {
+     finetunedDiagram.className = "hidden";
+  }
+  finetunedDiagram.src = newImageUrl;
+};
+window.addEventListener("scroll", onCaseScroll);
